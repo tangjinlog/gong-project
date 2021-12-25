f3236d522ea7f3aabcba9c1309389bdab750f538
Tang Jin <tangjin@tangjin.local>
2021-12-25 20:17:52 +0900

    db GET append

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ $(document).ready(()=> {
     medium: ['dodgerblue', 15, 7.5, 4000],
     large: ['yellowgreen', 30, 15, 5000]
   }
-  
+
   let gameOn = false;
   let t = 0;
   let mouseX;
@@ -44,7 +44,7 @@ $(document).ready(()=> {
       createCircle();
     })
   })
-  
+
   //공 생성
   function createCircle() {
     circleNumber++;
@@ -78,7 +78,7 @@ $(document).ready(()=> {
     //공 형태 생성
     let newCircle = `<div class="circle" id="${circleName}"></div>`;
     $('body').append(newCircle);
-    
+
     //css
     $(`#${circleName}`).css({
       'background-color' : circleColor,
@@ -94,11 +94,11 @@ $(document).ready(()=> {
       setTimeout(()=> {
         let currentCirclePosition = $(circleTrackId).position();
         let calculateRadius = parseInt($(circleTrackId).css('width')) * 0.5
-        
+
         //마우스와 공과의 거리 계산
         let distanceX = mouseX - (currentCirclePosition.left + calculateRadius);
         let distanceY = mouseY - (currentCirclePosition.top + calculateRadius);
-  
+
         //거리를 바탕으로 범위안에 들어오면 충돌 감지
         if(Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2)) <= calculateRadius) {
           $(circleTrackId).removeClass('circle').addClass('redcircle');
@@ -127,7 +127,7 @@ $(document).ready(()=> {
     //공이 움직일 수 있는 범위
     let circleMovePositionLeft = (moveableWidth * Math.random()).toFixed();
     let circleMovePositionTop = (moveableHeight * Math.random()).toFixed();
-    
+
     $(`#${circleId}`).animate({
       left: circleMovePositionLeft,
       top: circleMovePositionTop
@@ -154,7 +154,7 @@ $(document).ready(()=> {
   let highScore3 = 0.00;
   let highScore4 = 0.00;
   let highScore5 = 0.00;
-  
+
   //update button
   // const updateBtn = `<input type="button" class="updateBtn">`;
   // const updateCss = {
@@ -195,35 +195,51 @@ $(document).ready(()=> {
       success : function (response) {
         if(response['result'] == 'success') {
           let scores = response['scores'];
-          let name = scores[scores.length -1]['name'];
-          let score = scores[scores.length -1]['score'];
-  
+          // let name = scores['name'];
+          let score = scores['score'];
+
+          //score 내림차순
+          let score1 = `<div class="score center" id="score1" style="display:inline-block;">${scores[0]['score']}</div>`;
+          let score2 = `<div class="score center" id="score2" style="display:inline-block;">${scores[1]['score']}</div>`;
+          let score3 = `<div class="score center" id="score3" style="display:inline-block;">${scores[2]['score']}</div>`;
+          let score4 = `<div class="score center" id="score4" style="display:inline-block;">${scores[3]['score']}</div>`;
+          let score5 = `<div class="score center" id="score5" style="display:inline-block;">${scores[4]['score']}</div>`;
+
+          let name1 = scores[0]['name'];
+          let name2 = scores[1]['name'];
+          let name3 = scores[2]['name'];
+          let name4 = scores[3]['name'];
+          let name5 = scores[4]['name'];
+
           // $(`#inputBox`).remove();
           // let temp_html = `<div class="score center"><h2>${name} ${score}</h2></div>`;
           // $(`#${redScore}`).html(temp_html);
 
           //점수판 로직을 바탕으로 받아온 점수를 highscores에 append
           //점수공간
-          let highScorePlace1 = `<div class="score center" id="score1"><h2>${highScore1.toFixed(2)}</h2></div>`;
-          let highScorePlace2 = `<div class="score center" id="score2"><h2>${highScore2.toFixed(2)}</h2></div>`;
-          let highScorePlace3 = `<div class="score center" id="score3"><h2>${highScore3.toFixed(2)}</h2></div>`;
-          let highScorePlace4 = `<div class="score center" id="score4"><h2>${highScore4.toFixed(2)}</h2></div>`;
-          let highScorePlace5 = `<div class="score center" id="score5"><h2>${highScore5.toFixed(2)}</h2></div>`;
-          
+          let highScorePlace1 = `<div class="score center" id="score1"><h2>${name1} ${score1}</h2></div>`;
+          let highScorePlace2 = `<div class="score center" id="score2"><h2>${name2} ${score2}</h2></div>`;
+          let highScorePlace3 = `<div class="score center" id="score3"><h2>${name3} ${score3}</h2></div>`;
+          let highScorePlace4 = `<div class="score center" id="score4"><h2>${name4} ${score4}</h2></div>`;
+          let highScorePlace5 = `<div class="score center" id="score5"><h2>${name5} ${score5}</h2></div>`;
+
           let inputBox = `<div id="inputBox" style="display: inline-block; margin-right: 5px;">
                             <input type="text" id="score_name" style="width: 100px;">
                             <input type="button" value="등록" onclick="update();">
                           </div>`
-          
+
 
           if(redScore == 'score1' || `${score}` < newScore) {
+
             $('#highscores').append(
               // `${name}+${score}`,
-              highScorePlace1,
+//              highScorePlace1,
+              `${score1}`,
               highScorePlace2,
               highScorePlace3,
               highScorePlace4,
               highScorePlace5,
+              // `${name1} ${score1}`,
               resetbutton
             )
             $(`#${redScore}`).before(inputBox);
@@ -299,7 +315,7 @@ $(document).ready(()=> {
       highScore3 = highScore2;
       highScore2 = highScore1;
       highScore1 = newScore;
-      
+
     }
     else if(newScore > highScore2) {
       redScore = 'score2';
@@ -324,19 +340,19 @@ $(document).ready(()=> {
       highScore5 = newScore;
     }
 
-    
+
     // //점수공간
     // let highScorePlace1 = `<div class="score center" id="score1"><h2>${highScore1.toFixed(2)}</h2></div>`;
     // let highScorePlace2 = `<div class="score center" id="score2"><h2>${highScore2.toFixed(2)}</h2></div>`;
     // let highScorePlace3 = `<div class="score center" id="score3"><h2>${highScore3.toFixed(2)}</h2></div>`;
     // let highScorePlace4 = `<div class="score center" id="score4"><h2>${highScore4.toFixed(2)}</h2></div>`;
     // let highScorePlace5 = `<div class="score center" id="score5"><h2>${highScore5.toFixed(2)}</h2></div>`;
-    
+
     // let inputBox = `<div id="inputBox" style="display: inline-block; margin-right: 5px;">
     //                   <input type="text" id="score_name" style="width: 100px;">
     //                   <input type="button" value="등록" onclick="update();">
     //                 </div>`
-    
+
 
     // if(redScore == 'score1') {
     //   $('#highscores').append(
@@ -436,7 +452,7 @@ function update() {
             // alert(response['msg']);
             // window.location.reload();
         }
-        
+
     }
   })
 
@@ -459,12 +475,17 @@ function update() {
 }
 
 
-//목록 보여주기 
+//목록 보여주기
 function listing() {
-  
+
 }
 
-/* 
+/*
 highScore 붙인다 조건에 맞춰서.
-  
+
+
+maxScore < newScore
+maxScore = newScore
+
+
 */
